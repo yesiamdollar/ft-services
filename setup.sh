28e04a5a74f05ab9cca4b0c1e9c27984f3ad2f31
Achraf Boutahri <aboutahr@e2r3p2.1337.ma>
2020-10-28 14:51:08 +0100

    working on https

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#minikube start --driver=virtualbox
+# minikube docker-env
+# eval $(minikube docker-env)
+
+# create image
+
+# enable metallb on minikube addons
+
+# minikube addons enable metallb
