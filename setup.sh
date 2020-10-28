18eab0ba0350bbc1aed3f2fb34d9ef81f01bfabb
Achraf Boutahri <aboutahr@e2r3p2.1337.ma>
2020-10-28 19:20:06 +0100

    setup.sh step 1

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
-#minikube start --driver=virtualbox
-# minikube docker-env
-# eval $(minikube docker-env)
+minikube start --driver=virtualbox
+docker-machine create space
+docker-machine env space
+eval $(docker-machine env space)
+minikube docker-env
+eval $(minikube docker-env)
 
 # create image
 
 # enable metallb on minikube addons
 
-# minikube addons enable metallb
+minikube addons enable metallb
+kubectl apply -f srcs/metallb/metallb-config.yaml
+
+
+docker build ./srcs/nginx-conf/ -t nginx-server
+kubectl create -f ./srcs/nginx-conf/nginx-conf.yaml
+
