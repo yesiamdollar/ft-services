03455b47f09f1ed58cd3242b2723e4845d60739e
Achraf Boutahri <aboutahr@e1r6p6.1337.ma>
2020-12-15 12:29:59 +0100

    mysql expoted

--- srcs/mysql-conf/setup.sh
+++ srcs/mysql-conf/setup.sh
@@ -1,26 +1,10 @@
 #!/bin/sh
-
-if [ ! -d "/run/mysqld" ]; then
-  mkdir -p /run/mysqld
-fi
-if [ -d /work/mysql ]; then
-  echo "MySQL directory already present, skipping creation"
-else
-  echo "MySQL data directory not found, creating DBs"
-
-  mysql_install_db --user=root
-
-  cat << EOF > tmpfile
-USE mysql;
-FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "toor" WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '';
-CREATE DATABASE IF NOT EXISTS 'db' CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'dollar'@'%' IDENTIFIED BY '1337';
-GRANT ALL ON *.* to 'dollar'@'%' IDENTIFIED BY '1337';
-EOF
-   echo "hello" > jj
-  /usr/bin/mysqld --user=root < tmpfile 
-fi
-exec /usr/bin/mysqld
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+mkdir /run/openrc/
+touch /run/openrc/softlevel
+rc-status
+rc-service mariadb start
+mysqladmin -u root password toor
+mysql -u root < /users.sql
+rc-service mariadb restart 
+tail -f /dev/null
