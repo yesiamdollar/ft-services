ef2fd6e3e8e60fb65676e775db5b8c4b5edfac56
Achraf Boutahri <aboutahr@e1r6p6.1337.ma>
2020-12-15 14:03:43 +0100

    pma

--- setup.sh
+++ setup.sh
@@ -16,7 +16,9 @@ kubectl apply -f srcs/metallb/metallb-config.yaml
 docker build ./srcs/nginx-conf/ -t nginx-server
 docker build ./srcs/wp-conf/ -t wp-server
 docker build ./srcs/mysql-conf/ -t mysql-server
+docker build ./srcs/pma-conf/ -t pma-server
 
 kubectl create -f ./srcs/nginx-conf/nginx-conf.yaml
 kubectl create -f ./srcs/wp-conf/wp-conf.yaml
 kubectl create -f ./srcs/mysql-conf/mysql-conf.yaml
+kubectl create -f ./srcs/pma-conf/pma-conf.yaml
