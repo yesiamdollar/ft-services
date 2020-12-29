d0cf73229b9ea1efdba0eaddc8ab8d6ea94ecf48
Achraf Boutahri <aboutahr@e2r3p2.1337.ma>
2020-12-29 17:26:33 +0100

    ftps

--- setup.sh
+++ setup.sh
@@ -11,13 +11,18 @@ eval $(minikube docker-env)
 
 
 minikube addons enable metallb
+sed -i '' s/CIP/$(minikube ip)/g "srcs/metallb/metallb-config.yaml"
 kubectl apply -f srcs/metallb/metallb-config.yaml
 
-
+sed -i '' s/CIP/$(minikube ip)/g "srcs/metallb/metallb-config.yaml"
+sed -i '' s/CIP/$(minikube ip)/g "srcs/nginx-conf/nginx.conf"
 docker build ./srcs/nginx-conf/ -t nginx-server
 docker build ./srcs/wp-conf/ -t wp-server
 docker build ./srcs/mysql-conf/ -t mysql-server
 docker build ./srcs/pma-conf/ -t pma-server
+
+sed -i '' s/$(minikube ip)/CIP/g "srcs/metallb/metallb-config.yaml"
+sed -i '' s/$(minikube ip)/CIP/g "srcs/nginx-conf/nginx.conf"
 
 kubectl create -f ./srcs/nginx-conf/nginx-conf.yaml
 kubectl create -f ./srcs/wp-conf/wp-conf.yaml
